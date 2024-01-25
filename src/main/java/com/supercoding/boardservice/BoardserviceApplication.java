e3e34e55c8c6a7067b771056187b682afd541de0
pang sunju <92684379+pang-i@users.noreply.github.com>
2024-01-25 23:42:54 +0900

    환경세팅 커밋

---
+++ src/main/java/com/supercoding/boardservice/BoardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.supercoding.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supercoding/boardservice/BoardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supercoding.boardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
