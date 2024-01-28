62e2549e92959f338a375439fb4ebb7a3de5684d
pang sunju <92684379+pang-i@users.noreply.github.com>
2024-01-29 00:12:40 +0900

    240129 방선주 [feat] jpa 설정 추가

---
+++ src/main/java/com/supercoding/boardservice/config/jdbcConfig.java
@@ -0,0 +1,34 @@
+package com.supercoding.boardservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+public class jdbcConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/boardservice?useUnicode=true&characterEncoding=utf8");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){ return new JdbcTemplate(dataSource()); }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+
+    }
+
+}
+
+
