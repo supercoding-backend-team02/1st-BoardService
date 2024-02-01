a55171299875817807c86cde61a800cf4222eba9
pang sunju <92684379+pang-i@users.noreply.github.com>
2024-02-01 22:29:00 +0900

    240201 🎟️로그인 시 토큰 포함 res 전달

--- src/main/java/com/supercoding/boardservice/service/UserService.java
+++ src/main/java/com/supercoding/boardservice/service/UserService.java
@@ -1,37 +1,53 @@
 package com.supercoding.boardservice.service;
 
+import com.supercoding.boardservice.config.JwtTokenProvider;
 import com.supercoding.boardservice.dto.UserDto;
 import com.supercoding.boardservice.repository.users.UserEntity;
 import com.supercoding.boardservice.repository.users.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService {
 
+    private final CustomUserDetailsService customUserDetailsService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
+    public String login(UserDto userDto) {
+        String email = userDto.getEmail();
+        String password = userDto.getPassword();
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
-        System.out.println("user" + user);
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        UserDetails userDetails;
+        try{
+            userDetails = customUserDetailsService.loadUserByUsername(email);
+        } catch (UsernameNotFoundException e) {
+            throw new UsernameNotFoundException("등록된 이메일이 아닙니다.");
+        }
+
+        boolean passwordCheck = passwordEncoder.matches(password, userDetails.getPassword());
+
+        if (!passwordCheck) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        return jwtTokenProvider.createToken(email);
     }
 
     public boolean signup(UserDto userDto) {
         String email = userDto.getEmail();
         String password = userDto.getPassword();
+
+        if (userRepository.existsByEmail(email)) {
+            throw new DataIntegrityViolationException("이미 존재하는 이메일입니다.");
+        }
 
         if (userRepository.existsByEmail(email)) {
             return false;
@@ -40,6 +56,11 @@ public class UserService implements UserDetailsService {
                                                 .email(email)
                                                 .password(passwordEncoder.encode(password))
                                                 .build());
+
+        return true;
+    }
+
+    public boolean logout() {
         return true;
     }
 }
